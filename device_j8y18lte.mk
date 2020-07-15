1343c4fa5743ba65317506bbefb1cdc346021b34
Ayush <ayushbisht5663@gmail.com>
2020-07-15 12:40:40 +0530

    j8y18lte: remove PREBUILT KERNEL

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -8,14 +8,6 @@ $(call inherit-product-if-exists, vendor/samsung/j8y18lte/j8y18lte-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/samsung/j8y18lte/overlay
 
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/samsung/j8y18lte/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
 
 $(call inherit-product, build/target/product/full.mk)
 
