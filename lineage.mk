cdba1ebd581321b756dfc9282601ac396b79141c
Ayush <ayushbisht5663@gmail.com>
2020-07-15 12:47:15 +0530

    j8y18lte: include device specific

--- lineage.mk
+++ lineage.mk
@@ -1,3 +1,7 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Release name
 PRODUCT_RELEASE_NAME := j8y18lte
 
