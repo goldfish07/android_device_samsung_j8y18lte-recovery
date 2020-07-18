56b1da62d1bdf700216573e24456b174f0bae315
Ayush <ayushbisht5663@gmail.com>
2020-07-18 08:08:46 +0530

    j8y18lte: add media Configuration [API]

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -98,6 +98,12 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/audio/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
+
 
 # Disable adb security
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
