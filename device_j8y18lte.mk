3c812860ab284b0aa03c37f87ee75b724e5fb3b1
Ayush <ayushbisht5663@gmail.com>
2020-07-18 07:03:06 +0530

    j8y18lte: add keylayouts & define configs

--- device_j8y18lte.mk
+++ device_j8y18lte.mk
@@ -18,6 +18,16 @@ PRODUCT_AAPT_PREF_CONFIG := xhdpi
 TARGET_SCREEN_HEIGHT := 1480
 TARGET_SCREEN_WIDTH := 720
 
+
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(DEVICE_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+    $(DEVICE_PATH)/keylayout/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl \
+    $(DEVICE_PATH)/keylayout/synaptics_dsxv26.kl:system/usr/keylayout/synaptics_dsxv26.kl \
+    $(DEVICE_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl \
+
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
