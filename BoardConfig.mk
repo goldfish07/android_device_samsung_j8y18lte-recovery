10ff55de746f2575a4adf276fdae475654c63b03
Ayush <ayushbisht5663@gmail.com>
2020-07-23 06:13:25 +0530

    j8y18lte: remove obsolute

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,7 +65,7 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $
 
 # Kernel toolchains
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN := prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432 #25
