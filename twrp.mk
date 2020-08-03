1b2ccf368f4443b248b6bb1d5008437d20172ed5
Ayush <ayushbisht5663@gmail.com>
2020-08-03 06:18:55 +0530

    j8y18lte: update lun

--- twrp.mk
+++ twrp.mk
@@ -1,8 +1,9 @@
 
 TARGET_RECOVERY_DEVICE_DIRS := $(DEVICE_PATH)/twrp
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dw3/gadget/lun.%d/file"
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_CRYPTFS_HW_PATH := "vendor/qcom/opensource/cryptfs_hw"
+
 RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
@@ -24,7 +25,7 @@ TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_DEVICE_VERSION := goldfish07(Ayush Bisht)
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-TW_EXCLUDE_DEFAULT_USB_INIT := true
+#TW_EXCLUDE_DEFAULT_USB_INIT := true
 
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
