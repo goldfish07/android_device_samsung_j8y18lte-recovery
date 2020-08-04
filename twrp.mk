69e93c6a6f2ea35b4a7f25ac4069c7d580d0a975
Ayush <ayushbisht5663@gmail.com>
2020-08-04 07:45:23 +0530

    j8y18lte: enable USB

--- twrp.mk
+++ twrp.mk
@@ -1,5 +1,5 @@
 
-TARGET_RECOVERY_DEVICE_DIRS := $(DEVICE_PATH)/twrp
+#TARGET_RECOVERY_DEVICE_DIRS := $(DEVICE_PATH)/twrp
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dwc3/gadget/lun%d/file"
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_CRYPTFS_HW_PATH := "vendor/qcom/opensource/cryptfs_hw"
@@ -28,12 +28,12 @@ TW_DEVICE_VERSION := goldfish07(Ayush Bisht)
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 #TW_EXCLUDE_DEFAULT_USB_INIT := true
 
-TW_HAS_MTP := true
-TW_MTP_DEVICE := /dev/usb_mtp_gadget
+#TW_HAS_MTP := true
+#TW_MTP_DEVICE := /dev/usb_mtp_gadget
 
 TW_EXCLUDE_TWRPAPP := true
 
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
-PRODUCT_COPY_FILES += $(DEVICE_PATH)/recovery.fstab:recovery/root/system/etc/recovery.fstab
+PRODUCT_COPY_FILES += $(DEVICE_PATH)/recovery.fstab:recovery/root/etc/recovery.fstab
