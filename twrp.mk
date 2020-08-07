18cdb67ab0934e3095c430fae837914ca145c4ee
Ayush <ayushbisht5663@gmail.com>
2020-08-07 11:09:32 +0530

    j8y18lte: changes for SkyHawk

--- twrp.mk
+++ twrp.mk
@@ -2,7 +2,7 @@
 #TARGET_RECOVERY_DEVICE_DIRS := $(DEVICE_PATH)/twrp
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dwc3/gadget/lun%d/file"
 TARGET_HW_DISK_ENCRYPTION := true
-TARGET_CRYPTFS_HW_PATH := "vendor/qcom/opensource/cryptfs_hw"
+TARGET_CRYPTFS_HW_PATH := "vendor/qcom/opensource/commonsys/cryptfs_hw"
 
 RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
@@ -24,8 +24,23 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-TW_DEVICE_VERSION := goldfish07(Ayush Bisht)
+TW_DEVICE_VERSION := goldfish07
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+# NOTE - Dont use '-' or blank spaces in flag values , otherwise it will create build errors or other bugs in recovery (Excluding SHRP_PATH,SHRP_REC). 
+# Path of your SHRP Tree
+SHRP_PATH := device/samsung/j8y18lte
+# Maintainer name *
+SHRP_MAINTAINER := goldfish07
+# Device codename *
+SHRP_DEVICE_CODE := j8y18lte
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := Treble
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A/B
+
+SHRP_FLASH := 0
+
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
 #TW_EXCLUDE_DEFAULT_USB_INIT := true
 
 #TW_HAS_MTP := true
